a7a65d0aa71208c60fa798401f9eb64fdaeb4959
DuStein <steindu@gmail.com>
2022-09-25 20:52:21 -0300

    nao conseguindo fazer funcionar tailwind

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*/.tsx'],
+  content: [
+    './src/**/*/*.tsx',
+    './index.html',
+  ],
   theme: {
     extend: {},
   },
