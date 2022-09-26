fe920a8a88863dc51b3521dcee94fdbef83c0b27
DuStein <steindu@gmail.com>
2022-09-26 20:02:49 -0300

    tailwind ok funcionando

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,26 @@
-import { MagnifyingGlassPlus } from 'phosphor-react'
 import './styles/main.css';
 // import './App.css'
+function Formulario() {
+  return(
+    <div className='grid'>
+      <input type="text" placeholder='Nome' />
+      <input type="text" placeholder='Plantao' />
+      <input type="text" placeholder='Funcional' />
+      <button className='bg-slate-800 text-gray-400 p-4'>Enviar</button>
+    </div>
+  )
+}
+
 function App() {
 
     return (
+      <div>
+        <h1 className='bg-slate-500 text-center p-2'>Troca Plantão App</h1>
 
-      <h1>Teste</h1>
-    
+        <Formulario />
+
+      </div>
+      
       )
 }
 
