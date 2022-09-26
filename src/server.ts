c2dac9ba89be2b9b9cd10ac35bd6d30acd2324ae
DuStein <steindu@gmail.com>
2022-09-25 21:18:52 -0300

    pausando home

--- src/server.ts
+++ src/server.ts
@@ -22,9 +22,23 @@ app.get('/jobs', async (req, res) => {
   const jobs = await prisma.job.findMany();
 
   res.json(jobs)
-
 });
 
+app.post('/users', async (req, res) => {
+
+  const { nome, plantao, funcional } = req.body;
+
+  const newUser = await prisma.user.create({
+    data: {
+      nome,
+      plantao,
+      funcional
+    }
+  })
+
+  console.log(newUser)
+
+})
 
 
 app.listen(3030, () => { console.log('Servidor ATIVO !')})
